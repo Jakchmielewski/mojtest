b012a5ea1be74afa5ea55d8ad90f1b248ef0071d
Jakchmielewski <kuba91821@outlook.com>
2019-01-22 16:16:55 +0100

    zmiana main na hello world

--- main.cpp
+++ main.cpp
@@ -5,28 +5,7 @@ using namespace std;
 
 int main()
 {
- int wysokosc,w=0,i=1,z=0;
- cout<<"Podaj wysokosc choinki:";
- cin>>wysokosc;
- w=wysokosc;
-for(int w=1;w<wysokosc;w++)
-{
-    for(i=1;i<wysokosc-w;i++)
-    {
-        cout<<" ";
-    }
-
-    for(z=0;z<w*2-1;z++)
-    {
-        cout<<"*";
-    }
-    cout<<endl;
-}
-for(int x=1;x<wysokosc-1;x++){
-    cout<<" ";
-
-}
-cout<<"*";
+ cout<<"Hello world!";
 
 
     return 0;
